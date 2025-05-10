04d58ec35ebcd32591d5ab0c3d88d5f9b8080d6a
Antoine Boiron <antoine.boiron@edu.Devinci.fr>
2025-05-10 11:55:09 +0200

    Update scrapeV2.sh

--- scrapeV2.sh
+++ scrapeV2.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Scraper EUR/USD via FastForex avec clé API perso
 
-#API_KEY="d9f9b7f68e-620d1636aa-sub8cr"
-API_KEY="M25AKAQAPI4XT2MS"
+API_KEY="d9f9b7f68e-620d1636aa-sub8cr"
 OUTPUT_FILE="data2.csv"
 
 cd /home/ubuntu/Projet-PGL
